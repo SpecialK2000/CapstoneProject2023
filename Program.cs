71ec3ba31be4490f7ae4544d50dca73d238fa2ed
Keirstin <Keirstin@DESKTOP-M6OUQ58>
2023-11-03 19:20:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using RecipeFinderLibrary;
+
+
+// Database class handles interactions with the database
+/*
+public class Database
+{
+    // Placeholder methods for database interactions
+    public bool IsUsernameTaken(string username) { return false; }
+    public void SaveUser(User user) { }
+    public User QueryUserByUsername(string username) { return null; }
+    public Recipe QueryRecipeByName(string name) { return null; }
+    public void SaveRecipe(Recipe recipe) { }
+    public void RemoveRecipe(Recipe recipe) { }
+    //public List<Recipe> QueryRecipesByIngredient(string ingredient) { return null; }
+    //public List<Recipe> QueryRecipesByCuisine(string cuisine) { return null; }
+    public List<Recipe> QueryRecipesByDietaryRestrictions(List<string> restrictions) { return null; }
+    //public List<Recipe> QueryRecipesByCookingTime(int maxTime) { return null; }
+    public List<Recipe> QueryBudgetConsciousRecipes(double maxBudget) { return null; }
+    public void SaveUserRecipe(User user, Recipe recipe) { }
+    public void RemoveUserRecipe(User user, Recipe recipe) { }
+    public List<Recipe> GetSavedRecipesForUser(User user) { return null; }
+}
+*/
+
+
+// UserInterface class manages user interactions
+public class UserInterface
+{
+    // Placeholder methods for user interface interactions
+    public void DisplayMainMenu() { }
+    public string GetUserInput() { return ""; }
+    public void DisplayRegistrationForm() { }
+    public (string, string) GetRegistrationInput() { return ("", ""); }
+    public void DisplayLoginForm() { }
+    public (string, string) GetLoginInput() { return ("", ""); }
+    public void DisplaySearchOptions() { }
+    public (string, string) GetSearchCriteria() { return ("", ""); }
+    public void DisplaySearchResults(List<Recipe> recipes) { }
+    public void DisplayRecipeDetails(Recipe recipe) { }
+    public void DisplayUserDashboard(User user) { }
+}
+
+// UserAuthenticationModule handles user authentication
+public class UserAuthenticationModule
+{
+    // Placeholder methods for user authentication
+    public string AuthenticateUser(string username, string password) { return ""; }
+    //public string HashFunction(string password) { return ""; }
+}
+
+// UserRegistrationModule handles user registration
+public class UserRegistrationModule
+{
+    // Placeholder methods for user registration
+    public string RegisterUser(string username, string password) { return ""; }
+    //public string HashFunction(string password) { return ""; }
+}
+
+// RecipeSearchModule handles recipe searching
+public class RecipeSearchModule
+{
+    // Placeholder methods for recipe searching
+    //public List<Recipe> SearchByIngredients(List<string> ingredients) { return null; }
+    //public List<Recipe> SearchByCuisine(string cuisine) { return null; }
+    public List<Recipe> SearchByDietaryRestrictions(List<string> restrictions) { return null; }
+    //public List<Recipe> SearchByCookingTime(int maxTime) { return null; }
+    public List<Recipe> SearchBudgetConscious(double maxBudget) { return null; }
+}
+
+// Main class serves as the entry point of the program
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Initialize components and start program execution
+        // Placeholder code
+    }
+}
+
